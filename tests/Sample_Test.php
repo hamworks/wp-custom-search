5bec0a0fb5efcd9502681ed13c1efadb67fdea3f
Hiroshi Urabe <urabe@ham.works>
2021-04-14 18:40:28 +0900

    run php test

---
+++ tests/Sample_Test.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Builder Tests.
+ *
+ * @package HAMWORKS\WP\Taxonomy
+ */
+
+/**
+ * Class Builder_Test
+ */
+class Sample_Test extends \WP_UnitTestCase {
+
+	/**
+	 * Test sample.
+	 *
+	 * @test
+	 */
+	public function test_true() {
+		$this->assertTrue( true );
+	}
+}
